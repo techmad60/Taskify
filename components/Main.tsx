b82a4b3811bbcff2e1053baa8dbb8d1cfb3ed5e1
Success Obasi <obasisuccess192@gmail.com>
2024-06-24 12:13:53 +0100

    nav init

--- components/Main.tsx
+++ components/Main.tsx
@@ -81,7 +81,7 @@ export default function Main() {
             <div className="flex flex-col items-start bg-[#D9D9D9] w-[275px] h-[370px] p-8 rounded-md mt-4 mb-12">
               <p className={`${stylex.className} text-6xl text-color-one`}>&quot;</p>
               <p className={`${poppinsFont.className} text-center pb-8 text-sm`}>Taskify transformed my hectic pharmacy school schedule into a well-organized plan, helping me manage classes, assignments, and study sessions effortlessly.</p>
-              <div className="flex justify-between items-center gap-8">
+              <div className="flex justify-between items-center gap-4">
                 <Image 
                   src={"/images/avatar-one.png"}
                   alt="Avatar"
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,71 @@
-import Link from "next/link"
-import Image from "next/image"
-import { interFont } from "@/fonts/fonts"
+'use client'
+import {useState, useEffect} from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import { interFont } from "@/fonts/fonts";
+
 export default function Navbar () {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const handleToggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  useEffect(() => {
+    // Add or remove a class to the body and html to disable/enable scrolling
+    const body = document.body;
+    const html = document.documentElement;
+
+    if (isNavOpen) {
+      body.style.overflow = 'hidden';
+      html.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = 'visible';
+      html.style.overflow = 'visible';
+    }
+    // Clean up the effect on component unmount
+    return () => {
+      body.style.overflow = 'visible';
+      html.style.overflow = 'visible';
+    };
+  }, [isNavOpen])
+
     return (
-        <nav className={`${interFont.className} bg-primary lg:flex justify-between items-center lg:static`} >
-          <div className="flex justify-between items-center">
-            <div className="flex items-center">
-                <Image 
-                className=""
-                src={"/images/logo.svg"}
-                alt="Taskify-logo"
-                width={24}
-                height={24}/>
-              <p className={`${interFont.className} text-white`}>Taskify</p>
-            </div>
-            <div  className="cursor-pointer lg:hidden">
+      <nav className={`${interFont.className} bg-primary lg:flex justify-between items-center lg:static`} >
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
               <Image 
-                src={"/images/bars.svg"}
-                alt="Bars"
-                width={24}
-                height={24}/>
-            </div>
+              className=""
+              src={"/images/logo.svg"}
+              alt="Taskify-logo"
+              width={24}
+              height={24}/>
+            <p className={`${interFont.className} text-white`}>Taskify</p>
           </div>
-        <div className="bg-[#D9D9D9] absolute top-0 right-0 h-screen flex flex-col items-center p-4 sm:p-8 w-1/2 lg:w-auto lg:flex-row lg:static lg:h-auto lg:bg-transparent lg:p-0">
-          <Image
-            className="lg:hidden" 
-            src={"/images/close-bar.svg"}
-            alt="Close-Bar"
-            width={30}
-            height={30}/>
-          <div className=" flex flex-col lg:flex-row text-center leading-10 my-12 lg:space-x-6 lg:static text-color-one lg:text-white">
+          <div className="cursor-pointer lg:hidden" onClick={handleToggleNav}>
+            <Image 
+              src={"/images/bars.svg"}
+              alt="Bars"
+              width={24}
+              height={24}/>
+          </div>
+        </div>
+        {isNavOpen && (
+        <div
+          className="fixed top-0 left-0 w-[25vw]  min-h-screen bg-black opacity-50 z-50 lg:hidden"
+          onClick={handleToggleNav} 
+        />
+      )}
+        <div className={`bg-[#D9D9D9] absolute top-0 right-0 h-screen flex flex-col items-center p-4 sm:p-8 w-3/4 lg:w-auto lg:flex-row lg:static lg:h-auto lg:bg-transparent lg:p-0 ${isNavOpen ? 'fixed' : 'hidden lg:flex'} `}>
+          <button onClick={handleToggleNav} className="lg:hidden">
+            <Image
+              className="" 
+              src={"/images/close-bar.svg"}
+              alt="Close-Bar"
+              width={30}
+              height={30}/>
+          </button>
+          
+          <div className=" flex flex-col text-center leading-10 my-12 text-color-one text-xl lg:flex-row lg:text-base lg:space-x-6 lg:static lg:text-white">
             <Link href="/" className=" hover:text-primary-color transition duration-150 hover:ease-in hover:border-b border-primary-color">
               Home
             </Link>
@@ -45,10 +81,10 @@ export default function Navbar () {
           </div>
 
           <div className="flex flex-col lg:hidden">
-            <Link href="/login" className="bg-primary-color px-4 py-1 mb-4  text-color-one rounded-sm hover:bg-sky-400 transition duration-150 hover:ease-in">
+            <Link href="/login" className="bg-primary-color px-4 py-1 mb-4  text-color-one rounded-sm hover:bg-sky-400 transition duration-150 hover:ease-in text-center text-xl">
               Login
             </Link>
-            <Link href="/signup" className=" text-primary-color border-2 border-primary-color rounded-sm px-4 py-1  hover:border-sky-400 hover:text-sky-400 transition duration-150 hover:ease-in">
+            <Link href="/signup" className=" text-primary-color border-2 border-primary-color rounded-sm px-4 py-1  hover:border-sky-400 hover:text-sky-400 transition duration-150 hover:ease-in text-xl">
               Signup
             </Link>
           </div>
