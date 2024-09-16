1ecf4f0cb4bdc3ee551058afa9e4de462604eb30
Techmad <techmad60@gmail.com>
2024-09-15 18:39:42 -0700

    fix width

--- app/signup/layout.tsx
+++ app/signup/layout.tsx
@@ -1,7 +1,7 @@
 // /app/signup/layout.tsx
 export default function SignupLayout({ children }: { children: React.ReactNode }) {
     return (
-      <div className="signup-container min-h-screen flex flex-col justify-center">
+      <div className="signup-container min-h-screen flex flex-col justify-center items-center">
         {children}
       </div>
     );
