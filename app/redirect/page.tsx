990502744b9c8e8427a80fc773bb1bbbacfebbdb
Techmad <techmad60@gmail.com>
2024-09-27 03:40:26 -0700

    grrrr

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -20,7 +20,7 @@ export default function RedirectPage () {
                 className="mt-24"
             />
             <div className={`${pixelify.className} flex flex-col justify-center items-center text-color-zero text-center`}>
-                <h1 className="font-medium text-2xl">You're almost there!</h1>
+                <h1 className="font-medium text-2xl">You&apos;re almost there!</h1>
                 <p className="mt-4" >Check your email for a verification token!</p>
             </div>
         </div>
