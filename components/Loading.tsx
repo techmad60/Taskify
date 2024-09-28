cd844088db418cd5f5238208dee8f99184dfdef5
Techmad <techmad60@gmail.com>
2024-09-28 16:29:51 -0700

    not just enough

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,3 +1,10 @@
-export default function Loading () {
-    return <div>Loading...</div>;
+// components/Loading.tsx
+"use client";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-primary-color"></div>
+    </div>
+  );
 }
